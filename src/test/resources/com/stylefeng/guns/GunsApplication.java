437538dfc168ad03c673c50e719e1f89cf0a70f9
majiafei <majiafei@dafycredit.com>
2018-03-16 14:08:09 +0800

    fyjygh

---
+++ src/test/resources/com/stylefeng/guns/GunsApplication.java
@@ -0,0 +1,26 @@
+package com.stylefeng.guns;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * SpringBoot方式启动类
+ *
+ * @author stylefeng
+ * @Date 2017/5/21 12:06
+ */
+@SpringBootApplication
+public class GunsApplication extends WebMvcConfigurerAdapter {
+
+    protected final static Logger logger = LoggerFactory.getLogger(GunsApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(GunsApplication.class, args);
+        logger.info("GunsApplication is success!");
+    }
+}
